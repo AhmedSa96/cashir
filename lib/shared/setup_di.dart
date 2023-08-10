41425842659ab698d7e47c112b4ee2accd18a617
Ahmed Salah <75364400+AhmedSa96@users.noreply.github.com>
2023-08-10 22:50:46 +0300

    put splash controller i DI

--- lib/shared/setup_di.dart
+++ lib/shared/setup_di.dart
@@ -1,3 +1,4 @@
+import 'package:cashir/modules/splash/controller/splash.controller.dart';
 import 'package:cashir/shared/services/http.service.dart';
 import 'package:cashir/shared/services/local_storage.service.dart';
 import 'package:cashir/shared/services/toastr.service.dart';
@@ -13,4 +14,5 @@ void setupDI() {
 
   // app controllers
   Get.put(AuthController());
+  Get.put(SplashController());
 }
